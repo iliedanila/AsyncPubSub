8e11b86ad0d19c47a7941da5dd2b8ac37567000c
Ilie Danila <danila.ilie@gmail.com>
2016-11-20 12:30:23 +0200

    added playground.

---
+++ playground/playground/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  playground
+//
+//  Created by Ilie Danila on 20/11/2016.
+//  Copyright © 2016 Ilie Danila. All rights reserved.
+//
+
+#include <iostream>
+#include "../../nodeLib/nodeLib/nodeLib.hpp"
+
+int main(int argc, const char * argv[])
+{
+    nodeLib node;
+    
+    node.HelloWorld("went ok.");
+    
+    return 0;
+}
