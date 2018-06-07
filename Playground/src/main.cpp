58b85bae665292881c54da68f44d6ed685ea8a47
Ilie Danila <danila.ilie@gmail.com>
2018-06-07 11:10:21 +0300

    WIP (for work)

--- Playground/src/main.cpp
+++ Playground/src/main.cpp
@@ -14,7 +14,7 @@
 
 using namespace boost::asio;
 using namespace ip;
-using namespace std::chrono_literals;
+// using namespace std::chrono_literals;
 
 void populateData(LogicalLayer::PublisherData& publisherData)
 {
--- cross-arm.cmake
+++ cross-arm.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR armv7l)
-set(CMAKE_C_COMPILER   arm-linux-gnueabi-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
+set(CMAKE_C_COMPILER   /opt/gemini/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-gcc)
+set(CMAKE_CXX_COMPILER /opt/gemini/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-g++)
 
